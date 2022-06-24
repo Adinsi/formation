54dee94a09025becd86e882853d434fe7eeb477c
Adinsi Abdias <adinsiabdias@gmail.com>
2022-06-24 02:07:21 +0100

    source code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Navigation from '../component/Navigation';
 import Portfolio from '../component/Portfolio';
 import Services from '../component/Services';
 import Team from '../component/Team';
-import '../styles/page/Home.scss'
+
 
 
 const Home = () => {
